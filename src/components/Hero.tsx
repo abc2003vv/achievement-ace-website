f30970b7ade5ed0ea42ca7d94b0bf886ada075a7
abc2003vv <abc2003vv@gmail.com>
2025-05-12 21:24:14 +0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Gamepad2, Images } from "lucide-react";
-import faceImage from "../Imagess/face.jpg";
+import faceImage from "../imagess/face.jpg";
 
 const Hero = () => {
   return (
